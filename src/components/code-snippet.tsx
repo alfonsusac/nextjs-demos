5fe3f47897124991db86cada5eb0b1c1fa925154
Alfonsus Ardani <alfonsusac@gmail.com>
2023-07-24 07:59:49 +0700

    Fixed breadcrumb key={}

--- src/components/code-snippet.tsx
+++ src/components/code-snippet.tsx
@@ -3,17 +3,24 @@ export default function CodeSnippet(p: {
   filepath: string,
   code: JSX.Element,
 }) {
-  const textspans = p.filepath.split('/')
+  const textspans = p.filepath
+    .split('/')
+    .reduce((acc, val, i) => {
+      if (i === 0) acc.push(val)
+      else acc.push('/', val)
+      return acc
+    }, [] as string[])
 
   return (
-    <div className="border border-zinc-800 rounded-lg">
-      <div className="p-3 text-xs text-zinc-400 px-4">
-        { textspans.map((t, i) =>
-          i === 0 ? <span key={ i }>{ t }</span> : 
-          <>
-            <span key={ 2*i }>/</span>
-            <span key={ 2*i+1 }>{ t }</span>
-          </>) }
+    <div className="border border-zinc-800 rounded-lg my-4">
+      <div className="p-3 text-xs text-zinc-400 px-4 flex gap-1">
+        {
+          textspans.map((t, i) =>
+            t === '/' ?
+              <span key={ i }>/</span> :
+              <span key={ i }>{ t }</span>
+          )
+        }
       </div>
       { p.code }
     </div>
