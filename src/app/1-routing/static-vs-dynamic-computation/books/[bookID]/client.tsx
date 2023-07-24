c0b754171d8272f7e1ac9cab49252eab4349fca9
Alfonsus Ardani <alfonsusac@gmail.com>
2023-07-24 07:47:18 +0700

    Delete client.tsx

--- src/app/1-routing/static-vs-dynamic-computation/books/[bookID]/client.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useParams } from 'next/navigation'
-
-export function ClientSideParams() {
-  const params = useParams()
-
-  return (<>{ params.bookID.replace('%20', ' ') }</>)
-}
