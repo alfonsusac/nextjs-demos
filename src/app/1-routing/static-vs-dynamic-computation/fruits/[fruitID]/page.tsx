dca28f8088aa9e81a517f4db96dc374d456c906c
Alfonsus Ardani <alfonsusac@gmail.com>
2023-07-24 11:14:59 +0700

    Delete page.tsx

--- src/app/1-routing/static-vs-dynamic-computation/fruits/[fruitID]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link"
-
-import CodeSnippet from "@/components/code-snippet"
-
-export default function Page(p: { params: any }) {
-  return <div className="p-4 border border-zinc-800 rounded-lg">
-    <h1 className="text-xl py-2">{ p.params.carID }</h1>
-  </div>
-}
