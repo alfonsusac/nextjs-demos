e78d8ed3133d7d5a695cc978eae42569d96d7bcf
Alfonsus Ardani <alfonsusac@gmail.com>
2023-07-24 15:48:24 +0700

    Update page.tsx

--- src/app/1-routing/static-vs-dynamic-computation/dogs/[dogID]/page.tsx
+++ src/app/1-routing/static-vs-dynamic-computation/dogs/[dogID]/page.tsx
@@ -16,9 +16,11 @@ export default async function Page(p: { params: any }) {
 
 
   return <div className="p-4 border border-zinc-800 rounded-lg">
-    <h1 className="text-xl py-2">🐶 { p.params.dogID }</h1>
+    <h1 className="text-xl pt-2 pb-1">🐶 { p.params.dogID }</h1>
 
-    <p>Generated { seconds } seconds late from parent layout.</p>
+    <div className="mb-2">
+      <span className="text-sm text-zinc-300 bg-zinc-900 p-1 px-2 rounded-md">Generated <b>{ seconds } seconds late</b> from parent layout.</span>
+    </div>
 
     <p>This page is generated statically. If it doesn&apos;t exist/haven&apos;t been generated it will be generated upon request, showing the Loading UI.</p>
     <p>The next time you visit this page, it won&apos;t be generated again and will use the build cache instead.</p>
