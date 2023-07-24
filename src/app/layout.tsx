d0a6f82230eaee531f7fb9abb5594acd27088d4e
Alfonsus Ardani <alfonsusac@gmail.com>
2023-07-24 13:21:13 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,6 +92,7 @@ const getDirs = cache(() => {
     console.info(readdirSync(process.cwd()))
     console.info(readdirSync(path.join(process.cwd(), '.next')))
     console.info(readFileSync(path.join(process.cwd(), '.next/app-build-manifest.json'), 'utf-8'))
+    console.info(JSON.parse(readFileSync(path.join(process.cwd(), '.next/app-build-manifest.json'), 'utf-8')))
     // console.info(readFileSync(path.join(process.cwd(), '.next/routes-manifest.json'), 'utf-8'))
     // console.info(readFileSync(path.join(process.cwd(), '.next/prerender-manifest.json'), 'utf-8'))
 
