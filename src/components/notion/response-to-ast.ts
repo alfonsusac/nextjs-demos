eab5d477ac970e0a97ba583fea47b2b8edcef8c2
Alfonsus Ardani <alfonsusac@gmail.com>
2023-08-13 19:07:21 +0700

    Added NodeTypes

--- src/components/notion/response-to-ast.ts
+++ src/components/notion/response-to-ast.ts
@@ -1,4 +1,4 @@
-import { BlockObjectResponse, ListBlockChildrenResponse, RichTextItemResponse } from "@notionhq/client/build/src/api-endpoints"
+import { AudioBlockObjectResponse, BlockObjectResponse, BookmarkBlockObjectResponse, BreadcrumbBlockObjectResponse, BulletedListItemBlockObjectResponse, CalloutBlockObjectResponse, CheckboxPropertyItemObjectResponse, ChildDatabaseBlockObjectResponse, ChildPageBlockObjectResponse, CodeBlockObjectResponse, ColumnBlockObjectResponse, ColumnListBlockObjectResponse, DividerBlockObjectResponse, EmbedBlockObjectResponse, EquationBlockObjectResponse, FileBlockObjectResponse, Heading1BlockObjectResponse, Heading2BlockObjectResponse, Heading3BlockObjectResponse, ImageBlockObjectResponse, LinkPreviewBlockObjectResponse, LinkToPageBlockObjectResponse, ListBlockChildrenResponse, NumberedListItemBlockObjectResponse, ParagraphBlockObjectResponse, PdfBlockObjectResponse, QuoteBlockObjectResponse, RichTextItemResponse, SyncedBlockBlockObjectResponse, TableBlockObjectResponse, TableOfContentsBlockObjectResponse, TableRowBlockObjectResponse, TemplateBlockObjectResponse, ToDoBlockObjectResponse, ToggleBlockObjectResponse, UnsupportedBlockObjectResponse, VideoBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints"
 import { notion } from "./data"
 
 
@@ -22,12 +22,12 @@ export async function convertChildrenToAST(
 
   await mapBlockListToAST(data.results, root)
 
-  
+
   async function mapBlockListToAST(list: ListBlockChildrenResponse['results'], currentNode: NotionASTNode) {
     console.info("Visiting " + currentNode.type)
-    
+
     const fetchChildQueue: { id: string, node: NotionASTNode }[] = []
-    
+
     await Promise.all(list.map(unknownblock => {
       return (async () => {
         if (!Object.hasOwn(unknownblock, 'type') || unknownblock.object !== 'block') {
@@ -80,9 +80,13 @@ export async function convertChildrenToAST(
     }))
   }
 
-  return { ast: root, unknowns: jsxOfUnknownType}
+  return { ast: root, unknowns: jsxOfUnknownType }
 }
 
+
+type Props =
+  | BulletedListItemBlockObjectResponse['bulleted_list_item']
+  | NumberedListItemBlockObjectResponse['numbered_list_item']
 
 
 
@@ -96,7 +100,9 @@ export class NotionASTNode {
   has_children?: true
   is_group?: true
   content?: RichTextItemResponse[]
-  props: { [key: string]: any } = {}
+  props: {
+    [key: string]: any
+  } = {}
   children: NotionASTNode[] = []
 
 
@@ -129,9 +135,314 @@ export class NotionASTNode {
     return {
       info: `${this.type} | ${this.has_children ? "hasChildren" : "noChildren"} | ${this.id}`,
       type: `${this.type} | parent:${this.parent?.type}`,
-      content: this.content?.map( c => c.plain_text ).join(''),
+      content: this.content?.map(c => c.plain_text).join(''),
       props: this.props,
       children: this.children,
     }
   }
+}
+
+type OmitRichText<T> = Omit<T, 'rich_text'>
+
+export type NodeTypes = {
+  'root': NotionASTNode & {
+    type: 'root'
+    props: {}
+  }
+
+  'list_item': NotionASTNode & {
+    type: 'list_item'
+    props:
+    & OmitRichText<BulletedListItemBlockObjectResponse['bulleted_list_item']>
+    & OmitRichText<NumberedListItemBlockObjectResponse['numbered_list_item']>
+    & OmitRichText<ToDoBlockObjectResponse['to_do']>
+  }
+  'bulleted_list_item': NotionASTNode & {
+    type: 'bulleted_list_item'
+    props: {}
+  }
+  'numbered_list_item': NotionASTNode & {
+    type: 'numbered_list_item'
+    props: {}
+  }
+  'to_do': NotionASTNode & {
+    type: 'to_do'
+    props: {}
+  }
+  'heading_1': NotionASTNode & {
+    type: 'heading_1'
+    props: OmitRichText<Heading1BlockObjectResponse['heading_1']>
+  }
+  'heading_2': NotionASTNode & {
+    type: 'heading_2'
+    props: OmitRichText<Heading2BlockObjectResponse['heading_2']>
+  }
+  'heading_3': NotionASTNode & {
+    type: 'heading_3'
+    props: OmitRichText<Heading3BlockObjectResponse['heading_3']>
+  }
+  'paragraph': NotionASTNode & {
+    type: 'paragraph'
+    props: OmitRichText<ParagraphBlockObjectResponse['paragraph']>
+  }
+  'code': NotionASTNode & {
+    type: 'code'
+    props: OmitRichText<CodeBlockObjectResponse['code']>
+  }
+  'toggle': NotionASTNode & {
+    type: 'toggle'
+    props: OmitRichText<ToggleBlockObjectResponse['toggle']>
+  }
+  'quote': NotionASTNode & {
+    type: 'quote'
+    props: OmitRichText<QuoteBlockObjectResponse['quote']>
+  }
+  'divider': NotionASTNode & {
+    type: 'divider'
+    props: OmitRichText<DividerBlockObjectResponse['divider']>
+  }
+
+  'equation': NotionASTNode & {
+    type: 'equation'
+    props: OmitRichText<EquationBlockObjectResponse['equation']>
+  }
+  'callout': NotionASTNode & {
+    type: 'callout'
+    props: OmitRichText<CalloutBlockObjectResponse['callout']>
+  }
+  'column_list': NotionASTNode & {
+    type: 'column_list'
+    props: OmitRichText<ColumnListBlockObjectResponse['column_list']>
+  }
+  'column': NotionASTNode & {
+    type: 'column'
+    props: OmitRichText<ColumnBlockObjectResponse['column']>
+  }
+  'table': NotionASTNode & {
+    type: 'table'
+    props: OmitRichText<TableBlockObjectResponse['table']>
+  }
+  'table_row': NotionASTNode & {
+    type: 'table_row'
+    props: OmitRichText<TableRowBlockObjectResponse['table_row']>
+  }
+  'bookmark': NotionASTNode & {
+    type: 'bookmark'
+    props: OmitRichText<BookmarkBlockObjectResponse['bookmark']>
+  }
+
+  'image': NotionASTNode & {
+    type: 'image'
+    props: OmitRichText<ImageBlockObjectResponse['image']>
+  }
+  'video': NotionASTNode & {
+    type: 'video'
+    props: OmitRichText<VideoBlockObjectResponse['video']>
+  }
+  'pdf': NotionASTNode & {
+    type: 'pdf'
+    props: OmitRichText<PdfBlockObjectResponse['pdf']>
+  }
+  'audio': NotionASTNode & {
+    type: 'audio'
+    props: OmitRichText<AudioBlockObjectResponse['audio']>
+  }
+
+  'file': NotionASTNode & {
+    type: 'file'
+    props: OmitRichText<FileBlockObjectResponse['file']>
+  }
+  'embed': NotionASTNode & {
+    type: 'embed'
+    props: OmitRichText<EmbedBlockObjectResponse['embed']>
+  }
+  'link_preview': NotionASTNode & {
+    type: 'link_preview'
+    props: OmitRichText<LinkPreviewBlockObjectResponse['link_preview']>
+  }
+
+
+  'synced_block': NotionASTNode & {
+    type: 'synced_block'
+    props: OmitRichText<SyncedBlockBlockObjectResponse['synced_block']>
+  }
+  'child_page': NotionASTNode & {
+    type: 'child_page'
+    props: OmitRichText<ChildPageBlockObjectResponse['child_page']>
+  }
+  'child_database': NotionASTNode & {
+    type: 'child_database'
+    props: OmitRichText<ChildDatabaseBlockObjectResponse['child_database']>
+  }
+  'breadcrumb': NotionASTNode & {
+    type: 'breadcrumb'
+    props: OmitRichText<BreadcrumbBlockObjectResponse['breadcrumb']>
+  }
+  'table_of_contents': NotionASTNode & {
+    type: 'table_of_contents'
+    props: OmitRichText<TableOfContentsBlockObjectResponse['table_of_contents']>
+  }
+  'link_to_page': NotionASTNode & {
+    type: 'link_to_page'
+    props: OmitRichText<LinkToPageBlockObjectResponse['link_to_page']>
+  }
+  'template': NotionASTNode & {
+    type: 'template'
+    props: OmitRichText<TemplateBlockObjectResponse['template']>
+  }
+  'unsupported': NotionASTNode & {
+    type: 'unsupported'
+    props: OmitRichText<UnsupportedBlockObjectResponse['unsupported']>
+  }
+
+}
+
+export type RootNode = NotionASTNode & {
+  type: 'root'
+  props: {}
+}
+
+export type ListItemNode = NotionASTNode & {
+  type: 'list_item'
+  props:
+  & OmitRichText<BulletedListItemBlockObjectResponse['bulleted_list_item']>
+  & OmitRichText<NumberedListItemBlockObjectResponse['numbered_list_item']>
+  & OmitRichText<ToDoBlockObjectResponse['to_do']>
+}
+export type BulletedListNode = NotionASTNode & {
+  type: 'bulleted_list_item'
+  props: {}
+}
+export type NumberedListNode = NotionASTNode & {
+  type: 'numbered_list_item'
+  props: {}
+}
+export type ToDoListNode = NotionASTNode & {
+  type: 'to_do'
+  props: {}
+}
+export type Heading1Node = NotionASTNode & {
+  type: 'heading_1'
+  props: OmitRichText<Heading1BlockObjectResponse['heading_1']>
+}
+export type Heading2Node = NotionASTNode & {
+  type: 'heading_2'
+  props: OmitRichText<Heading2BlockObjectResponse['heading_2']>
+}
+export type Heading3Node = NotionASTNode & {
+  type: 'heading_3'
+  props: OmitRichText<Heading3BlockObjectResponse['heading_3']>
+}
+export type ParagraphNode = NotionASTNode & {
+  type: 'paragraph'
+  props: OmitRichText<ParagraphBlockObjectResponse['paragraph']>
+}
+export type CodeNode = NotionASTNode & {
+  type: 'code'
+  props: OmitRichText<CodeBlockObjectResponse['code']>
+}
+export type ToggleNode = NotionASTNode & {
+  type: 'toggle'
+  props: OmitRichText<ToggleBlockObjectResponse['toggle']>
+}
+export type QuoteNode = NotionASTNode & {
+  type: 'quote'
+  props: OmitRichText<QuoteBlockObjectResponse['quote']>
+}
+export type DividerNode = NotionASTNode & {
+  type: 'divider'
+  props: OmitRichText<DividerBlockObjectResponse['divider']>
+}
+
+export type EquationNode = NotionASTNode & {
+  type: 'equation'
+  props: OmitRichText<EquationBlockObjectResponse['equation']>
+}
+export type CalloutNode = NotionASTNode & {
+  type: 'callout'
+  props: OmitRichText<CalloutBlockObjectResponse['callout']>
+}
+export type ColumnListNode = NotionASTNode & {
+  type: 'column_list'
+  props: OmitRichText<ColumnListBlockObjectResponse['column_list']>
+}
+export type ColumnNode = NotionASTNode & {
+  type: 'column'
+  props: OmitRichText<ColumnBlockObjectResponse['column']>
+}
+export type TableNode = NotionASTNode & {
+  type: 'table'
+  props: OmitRichText<TableBlockObjectResponse['table']>
+}
+export type TableRowNode = NotionASTNode & {
+  type: 'table_row'
+  props: OmitRichText<TableRowBlockObjectResponse['table_row']>
+}
+export type BookmarkNode = NotionASTNode & {
+  type: 'bookmark'
+  props: OmitRichText<BookmarkBlockObjectResponse['bookmark']>
+}
+
+export type ImageNode = NotionASTNode & {
+  type: 'image'
+  props: OmitRichText<ImageBlockObjectResponse['image']>
+}
+export type VideoNode = NotionASTNode & {
+  type: 'video'
+  props: OmitRichText<VideoBlockObjectResponse['video']>
+}
+export type PdfNode = NotionASTNode & {
+  type: 'pdf'
+  props: OmitRichText<PdfBlockObjectResponse['pdf']>
+}
+export type AudioNode = NotionASTNode & {
+  type: 'audio'
+  props: OmitRichText<AudioBlockObjectResponse['audio']>
+}
+
+export type FileNode = NotionASTNode & {
+  type: 'file'
+  props: OmitRichText<FileBlockObjectResponse['file']>
+}
+export type EmbedNode = NotionASTNode & {
+  type: 'embed'
+  props: OmitRichText<EmbedBlockObjectResponse['embed']>
+}
+export type LinkPreviewNode = NotionASTNode & {
+  type: 'link_preview'
+  props: OmitRichText<LinkPreviewBlockObjectResponse['link_preview']>
+}
+
+
+export type SyncedBlockNode = NotionASTNode & {
+  type: 'synced_block'
+  props: OmitRichText<SyncedBlockBlockObjectResponse['synced_block']>
+}
+export type ChildPageNode = NotionASTNode & {
+  type: 'child_page'
+  props: OmitRichText<ChildPageBlockObjectResponse['child_page']>
+}
+export type ChildDatabaseNode = NotionASTNode & {
+  type: 'child_database'
+  props: OmitRichText<ChildDatabaseBlockObjectResponse['child_database']>
+}
+export type BreadCrumbNode = NotionASTNode & {
+  type: 'breadcrumb'
+  props: OmitRichText<BreadcrumbBlockObjectResponse['breadcrumb']>
+}
+export type TableOfContentsNode = NotionASTNode & {
+  type: 'table_of_contents'
+  props: OmitRichText<TableOfContentsBlockObjectResponse['table_of_contents']>
+}
+export type LinkToPageNode = NotionASTNode & {
+  type: 'link_to_page'
+  props: OmitRichText<LinkToPageBlockObjectResponse['link_to_page']>
+}
+export type TemplateNode = NotionASTNode & {
+  type: 'template'
+  props: OmitRichText<TemplateBlockObjectResponse['template']>
+}
+export type UnsupportedNode = NotionASTNode & {
+  type: 'unsupported'
+  props: OmitRichText<UnsupportedBlockObjectResponse['unsupported']>
 }
