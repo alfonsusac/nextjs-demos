588341051b985e300adbdd11a243402525b01d45
Alfonsus Ardani <alfonsusac@gmail.com>
2023-08-14 16:29:44 +0700

    Exempty article from adding header

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link"
 import { titleCase } from "title-case"
-import { usePathname } from 'next/navigation'
+import { usePathname, useSelectedLayoutSegment } from 'next/navigation'
 import { useTOC } from "@/components/toc/context"
 import { slug } from "github-slugger"
 import { TOCItemType } from "@/components/toc/rsc"
@@ -52,30 +52,39 @@ export function Article(p: {
   children: React.ReactNode
 }) {
   let title = usePathname()?.split('/').at(2)?.replace(/[0-9]-/, '')
-  if(!title) title = ""
+  if (!title) title = ""
+  
+  const segment = useSelectedLayoutSegment()
+
   
   
   return (
-    <article className="p-4 w-full min-w-0">
+    <>
+      <article className="p-4 w-full min-w-0">
 
-      <header className="pt-4 pb-4">
-        <h1>{ titleCase(title.replaceAll('-', ' ')) }</h1>
-      </header>
+        {
+          segment !== 'articles' ? (
+            <header className="pt-4 pb-4">
+              <h1>{ titleCase(title.replaceAll('-', ' ')) }</h1>
+            </header>
+          ) : null
+        }
 
-      { p.children }
+        { p.children }
 
-      <footer className="mt-12 py-12 border-t border-t-zinc-600 text-zinc-500">
-        <p>
-          The content on this website are purely written by Alfon to help people better understand how Next.js works and are not affiliated with Vercel (unofficial).
-        </p>
-        <p>
-          If you have any comments for improvement on the website or the content feel free to visit <a href="https://github.com/alfonsusac/nextjs-demos/issues">the respository</a> which is 100% open source.
-        </p>
-        <p>
-          Written by <a href="https://github.com/alfonsusac">@alfonsusac</a>
-        </p>
-      </footer>
+        <footer className="mt-12 py-12 border-t border-t-zinc-600 text-zinc-500 text-sm space-y-2 leading-normal">
+          <p>
+            The content on this website are purely written by Alfon to help people better understand how Next.js works and are not affiliated with Vercel (unofficial).
+          </p>
+          <p>
+            If you have any comments for improvement on the website or the content feel free to visit <a href="https://github.com/alfonsusac/nextjs-demos/issues">the respository</a> which is 100% open source.
+          </p>
+          <p>
+            Written by <a href="https://github.com/alfonsusac">@alfonsusac</a>
+          </p>
+        </footer>
 
-    </article>
+      </article>
+    </>
   )
 }
