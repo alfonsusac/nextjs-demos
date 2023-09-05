bd56becb39132eaae711674e0bf37d9bc209ae08
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-05 10:47:50 +0700

    Added Audit

--- src/components/image/placeholder.tsx
+++ src/components/image/placeholder.tsx
@@ -2,38 +2,43 @@ import fetch2 from 'node-fetch'
 import { getPlaiceholder } from "plaiceholder"
 import sizeof from 'object-sizeof'
 import { unstable_cache } from 'next/cache'
+import { Audit } from '../timer'
 
 
 export async function getImage(src: string) {
 
-  return (
-    unstable_cache(
-      async () => {
-        const buffer = await fetch2(src).then(async (res) => Buffer.from(await res.arrayBuffer()))
+  const audit = new Audit('Image Component', false)
+  const cache = unstable_cache(
+    async () => {
+      const buffer = await fetch2(src).then(async (res) => Buffer.from(await res.arrayBuffer()))
 
-        const {
-          metadata: { height, width },
-          ...plaiceholder
-        } = await getPlaiceholder(buffer, {
-          size: 10
-        },)
+      const {
+        metadata: { height, width },
+        ...plaiceholder
+      } = await getPlaiceholder(buffer, {
+        size: 10
+      },)
 
 
-        const res = {
-          base64: plaiceholder.base64,
-          img: { src, height, width },
-        }
-
-        console.log("Size of GetImage Cached: " + sizeof(res) + " Bytes")
-
-        return res
-      },
-      ['image', src],
-      {
-        tags: ['image', src]
+      const res = {
+        base64: plaiceholder.base64,
+        img: { src, height, width },
       }
-    )()
-  )
+
+      console.log("Size of GetImage Cached: " + sizeof(res) + " Bytes")
+
+      audit.mark('Cache Miss Retrieving')
+
+      return res
+    },
+    ['image', src],
+    {
+      tags: ['image', src]
+    }
+  )();
+  audit.total()
+
+  return cache
 
 }
 
