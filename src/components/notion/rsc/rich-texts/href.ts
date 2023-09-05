03dc39b2d1a94d40b67e54d2ece2d420a6876827
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-05 14:57:42 +0700

    Update href.ts

--- src/components/notion/rsc/rich-texts/href.ts
+++ src/components/notion/rsc/rich-texts/href.ts
@@ -3,21 +3,26 @@ import { slug } from "github-slugger"
 import { unstable_cache } from "next/cache"
 import notion from "@/lib/notion"
 import { flattenRichText } from "./utils"
+import { Audit } from "@/components/timer"
 
 export async function parseNotionHref(c: string) {
   if (c.startsWith('/')) {
     const blockid = c.split('#')[1]
     try {
 
+      const audit = new Audit('', false)
       const blockdata = await unstable_cache(
         async () => await notion.blocks.retrieve({
           block_id: blockid
-        }) as any, [blockid]
+        }) as any,
+        [blockid]
       )()
-
+      
       const rich_text = blockdata[blockdata.type!].rich_text as RichTextItemResponse[]
       const text = flattenRichText(rich_text)!
       const anchorlink = `#${slug(text)}`
+      audit.mark('  Parsing Notion Href')
+
       return anchorlink
     } catch (error) {
       console.log(error)
