c37e14616bcf356ff8452d7047f57680099beadc
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-07 23:30:00 +0700

    Fix page mention without anchor

--- src/components/notion/rsc/rich-texts/href.ts
+++ src/components/notion/rsc/rich-texts/href.ts
@@ -7,29 +7,38 @@ import { Audit } from "@/components/timer"
 
 export async function parseNotionHref(c: string) {
   if (c.startsWith('/')) {
+
     const blockid = c.split('#')[1]
-    try {
-
-      const audit = new Audit('', false)
-      const blockdata = await unstable_cache(
-        async () => await notion.blocks.retrieve({
-          block_id: blockid
-        }) as any,
-        [blockid]
-      )()
-      
-      const rich_text = blockdata[blockdata.type!].rich_text as RichTextItemResponse[]
-      const text = flattenRichText(rich_text)!
-      const anchorlink = `#${slug(text)}`
-      audit.mark('  Parsing Notion Href')
-
-      return anchorlink
-    } catch (error) {
-      console.log(error)
-      return c
+    if (blockid) {
+      try {
+        return await getNotionAnchorHref(blockid)
+      } catch (error) {
+        console.log("ERROR: error parsing notion href: " + c)
+        console.log(error)
+        return c
+      }
+    } else {
+      return `https://alfonsusardani.notion.site${c}`
     }
   } else {
     return c
   }
 }
 
+
+async function getNotionAnchorHref(blockid: string) {
+  const audit = new Audit(' Parsing Notion Anchor Href', false)
+  
+  const blockdata = await unstable_cache(
+    async () => await notion.blocks.retrieve({
+      block_id: blockid
+    }) as any,
+    [blockid]
+  )()
+
+  const rich_text = blockdata[blockdata.type!].rich_text as RichTextItemResponse[]
+  const text = flattenRichText(rich_text)!
+  const anchorlink = `#${slug(text)}`
+  audit.total()
+  return anchorlink
+}
