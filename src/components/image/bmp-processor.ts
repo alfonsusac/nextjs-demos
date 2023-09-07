0c494b7bc2fce4610079fb98c213a61988093433
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-07 23:19:34 +0700

    Create bmp-processor.ts

---
+++ src/components/image/bmp-processor.ts
@@ -0,0 +1,9 @@
+import Jimp from "jimp"
+
+export async function convertBMPtoPNG(buffer: Buffer) {
+  
+  const img = await Jimp.read(buffer)
+  const png = await img.getBufferAsync(Jimp.MIME_PNG)
+  return png
+
+}
