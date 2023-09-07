d158c6df053ea5f5c54cddf66fbbe75838893141
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-07 23:22:29 +0700

    Update headings.tsx

--- src/components/notion/rsc/components/headings.tsx
+++ src/components/notion/rsc/components/headings.tsx
@@ -1,4 +1,4 @@
-import { H2, H3, H4 } from "@/components/typography"
+import { H2, H3, H4, cn } from "@/components/typography"
 import { Toggle } from "../../client"
 import { NotionComponentProp } from "../notion-ast-renderer-2"
 import { RichTextNode } from "./common"
@@ -15,24 +15,26 @@ export function HeadingBuilder(level: 1 | 2 | 3) {
 
     const slugid = slug(flattenRichText(node.props.rich_text))
 
-    const InnerHeading = level === 1 ? (
-      <H2 className={ className } id={ slugid }>
-        <RichTextNode node={ node } />
-      </H2>
-    ) : level === 2 ? (
-      <H3 className={ className } id={ slugid }>
-        <RichTextNode node={ node } />
-      </H3>
-    ) : level === 3 ? (
-      <H4 className={ className } id={ slugid }>
-        <RichTextNode node={ node } />
-      </H4>
-    ) : <></>
-
     return node.props.is_toggleable ? (
 
       <Toggle className="toggle-heading1"
-        headerSlot={ InnerHeading }>
+        headerSlot={
+
+          level === 1 ? (
+            <H2 className={ cn(className, 'my-0') } id={ slugid }>
+              <RichTextNode node={ node } />
+            </H2>
+          ) : level === 2 ? (
+            <H3 className={ cn(className, 'my-0') } id={ slugid }>
+              <RichTextNode node={ node } />
+            </H3>
+          ) : level === 3 ? (
+            <H4 className={ cn(className, 'my-0') } id={ slugid }>
+              <RichTextNode node={ node } />
+            </H4>
+          ) : <></>
+
+        }>
         <div className="pl-4">
           { children }
         </div>
@@ -40,7 +42,19 @@ export function HeadingBuilder(level: 1 | 2 | 3) {
 
     ) : (
 
-      InnerHeading
+      level === 1 ? (
+        <H2 className={ cn(className) } id={ slugid }>
+          <RichTextNode node={ node } />
+        </H2>
+      ) : level === 2 ? (
+        <H3 className={ cn(className) } id={ slugid }>
+          <RichTextNode node={ node } />
+        </H3>
+      ) : level === 3 ? (
+        <H4 className={ cn(className) } id={ slugid }>
+          <RichTextNode node={ node } />
+        </H4>
+      ) : <></>
 
     )
 
