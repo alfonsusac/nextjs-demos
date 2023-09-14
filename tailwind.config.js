9e691f265515b2a17c69c0e8292258753a4442a7
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-14 09:56:21 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,7 +48,7 @@ module.exports = {
         contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
       maxWidth: {
-        article: '36rem'
+        article: '40rem'
       },
     },
     container: {
