ebf019b0fd1e09f83eb668700a952144ddd23274
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-14 10:23:04 +0700

    fixed package.json (again)

--- src/components/image/bmp-processor.ts
+++ src/components/image/bmp-processor.ts
@@ -1,9 +1,7 @@
 import Jimp from "jimp"
 
 export async function convertBMPtoPNG(buffer: Buffer) {
-  
   const img = await Jimp.read(buffer)
   const png = await img.getBufferAsync(Jimp.MIME_PNG)
   return png
-
 }
