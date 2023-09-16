0d7e02e2a60490d42b13d4c5e77a5112191dc331
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-16 15:27:12 +0700

    Update page.tsx

--- src/app/articles/[slug]/revalidate/page.tsx
+++ src/app/articles/[slug]/revalidate/page.tsx
@@ -1,7 +1,8 @@
 import { redirect } from "next/navigation"
-import { revalidateTag } from 'next/cache'
+import { revalidatePath, revalidateTag } from 'next/cache'
 
 export default function RevalidatePage({params}: { params: { slug: string }}) {
   revalidateTag(params.slug)
+  revalidatePath(`/articles/${params.slug}`)
   redirect(`/articles/${params.slug}`)
 }
