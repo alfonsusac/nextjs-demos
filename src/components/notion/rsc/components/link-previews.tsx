76b688a1ef9856125da9c798c80c17e1d926c91b
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-16 15:27:05 +0700

    Update link-previews.tsx

--- src/components/notion/rsc/components/link-previews.tsx
+++ src/components/notion/rsc/components/link-previews.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { getMetaInfo } from "@/components/metadata/util"
 import { NotionComponentProp } from "../notion-ast-renderer-2"
 import { cn } from "@/components/typography"
@@ -5,75 +6,129 @@ import { CaptionNode } from "./common"
 import { Audit } from "@/components/timer"
 import Link from "next/link"
 import Image from "next/image"
+import React, { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 
 export async function LinkBookmark(P: {
-  url: string,
+  loading?: boolean,
+  url?: string,
   title?: string,
   description?: string,
   faviconpath?: string,
-  thumbnail?:string,
+  thumbnail?: string,
   className?: string
   variant?: "1" | "2"
 }) {
-  const { variant = "1" } = P
-  
-  const metadata = !P.title ? await getMetaInfo(P.url) : {
+  const url = P.url ?? null
+  const metadata = !P.title && P.url ? await getMetaInfo(P.url) : {
     title: P.title,
     description: P.description,
     faviconpath: P.faviconpath,
     url: P.url
-  } 
+  }
+
+  function Outer(R: { children: React.ReactNode }) {
+    return (
+      !P.loading && P.url ? (
+        <Link
+          target="_blank"
+          href={ P.url }
+          className={ cn(P.className,
+            "w-full h-32",
+            "flex flex-row items-stretch",
+            "border",
+            "no-underline",
+            "group relative overflow-hidden transition-all",
+
+            "border-slate-500/10",
+            "bg-gradient-to-bl from-slate-950/30 shadow-inner",
+            "hover:border-slate-800",
+
+            "rounded-lg",
+
+            "duration-100",
+            // "hover:scale-[1.01]",
+          ) }
+        >
+          { R.children }
+        </Link>
+      ) : (
+        <div
+          className={ cn(P.className,
+            "w-full h-32",
+            "flex flex-row items-stretch",
+            "border",
+            "no-underline",
+            "group relative overflow-hidden transition-all",
+
+            "border-slate-500/10",
+            "bg-gradient-to-bl from-slate-950/30 shadow-inner",
+            "hover:border-slate-800",
+
+            "rounded-lg",
+
+            "duration-100",
+          ) }
+        >
+          { R.children }
+        </div>
+      )
+    )
+  }
+
+
   return (
-    <Link
-      target="_blank"
-      href={ P.url }
-      className={ cn(P.className,
-        "w-full",
-        "flex flex-row items-stretch",
-        "border",
-        "no-underline",
-        "group relative overflow-hidden transition-all",
-
-        "border-slate-500/10",
-        "bg-gradient-to-bl from-slate-950/30 shadow-inner",
-        "hover:border-slate-800",
-
-        "rounded-lg",
-
-        "duration-100",
-        // "hover:scale-[1.01]",
-      ) }
-    >
+    <Outer>
+      {/* Hover */ }
       <div className={ cn(
-        "block opacity-0 absolute w-full h-full  transition-all duration-150",
+        "opacity-0 absolute  w-full h-full  transition-all duration-150",
         "group-hover:bg-slate-800/50",
         "group-hover:opacity-50"
       ) } />
-      {/* LEFT */}
-      <div className="p-4 w-full">
-        <div className="text-slate-200 truncate w-full">
-          { metadata.title }<span className="">
+      {/* LEFT */ }
+      <div className="p-4 pt-3">
+        <div className={ cn("text-slate-200 truncate w-full h-[1lh]") }>
+          {
+            P.loading ? (
+              <div className="w-36 bg-slate-700/50 rounded-lg  animate-pulse h-[0.7lh]   mt-1 text-transparent">
+                Loading...
+              </div>
+            ) : null
+          }
+          { metadata.title }
+          {
+            metadata.url &&
             <svg className="inline align-text-top text-slate-400 " xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M7 7h10m0 0v10m0-10L7 17"></path></svg>
-          </span>
+          }
         </div>
 
-        { // DESCRIPTION
-          metadata.description ? (
-            <div className={ cn(
-              "text-sm text-slate-400",
-              "mt-1 mb-2 h-10 w-full",
-              "line-clamp-2"
-            ) }>
-              { metadata.description }
-            </div>
-          ) : null
-        }
+
+        <div className={ cn(
+          "text-sm text-slate-400",
+          "mt-1 mb-2 h-10 w-full",
+          "line-clamp-2"
+        ) }>
+          {
+            P.loading ? (
+              <div className="w-96 bg-slate-700/50 rounded-lg  animate-pulse h-[0.7lh]   mt-1 text-transparent">
+                Loading...
+              </div>
+            ) : null
+          }
+          { metadata.description }
+        </div>
         {/* FOOTER */ }
         <div className="text-sm text-slate-500 flex flex-row gap-2">
-
           {
-            metadata.faviconpath ? (
-              <div className="flex items-center">
+            P.loading ? (
+              <div className="w-48 bg-slate-700/50 rounded-lg  animate-pulse h-[0.7lh]   mt-1 text-transparent">
+                Loading...
+              </div>
+            ) : null
+          }
+          <div className="flex items-center">
+            {
+              !P.loading && metadata.faviconpath ? (
                 <Image
                   unoptimized
                   width="16"
@@ -82,11 +137,11 @@ export async function LinkBookmark(P: {
                   alt="Link Icon URL"
                   className="w-4 h-4"
                 />
-              </div>
-            ) : null
-          }
+              ) : null
+            }
+          </div>
           <div /* LINK */ >
-            { metadata.url ? (new URL(metadata.url)).host : P.url }
+            { metadata.url ? (new URL(metadata.url)).host : P.url }{ " " }
           </div>
         </div>
       </div>
@@ -101,12 +156,12 @@ export async function LinkBookmark(P: {
               className="h-full object-cover"
             />
           </div>
-        ): (
+        ) : (
           <></>
         )
       }
-    </Link>
-   )
+    </Outer>
+  )
 }
 
 export async function NotionLinkBookmark({
@@ -116,12 +171,19 @@ export async function NotionLinkBookmark({
 
   // console.log("LinkBookmark Async")
   const audit = new Audit('', false)
-  const metadata = await getMetaInfo(node.props.url)
   audit.mark('Link Bookmark Component')
 
   return (
     <div className="my-2">
-      <LinkBookmark url={ node.props.url } className={ className } />
+      <ErrorBoundary fallback={
+        <LinkBookmark url="" loading />
+      }>
+        <Suspense fallback={
+          <LinkBookmark url="" loading />
+        }>
+          <LinkBookmark url={ node.props.url } className={ className } />
+        </Suspense>
+      </ErrorBoundary>
       <CaptionNode node={ node } />
     </div>
   )
