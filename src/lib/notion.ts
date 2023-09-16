c0f4312a7f9081176f8b2dfdb62c95b59197982f
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-16 15:16:05 +0700

    Update notion.ts

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,8 +1,17 @@
-import { Client } from "@notionhq/client"
+import { Client, LogLevel } from "@notionhq/client"
+import chalk from "chalk"
 
 const notionClientSingleton = () => {
   // console.log("Creating new Notion Client...")
-  return new Client({ auth: process.env.NOTION_TOKEN })
+  return new Client({
+    auth: process.env.NOTION_TOKEN,
+    timeoutMs: 5000,
+    logLevel: LogLevel.DEBUG, 
+    logger(level: LogLevel, message: string, extraInfo: Record<string, unknown>) {
+      console.log(`Notion: ${chalk.cyan(level)} ${message}`)
+      console.log(chalk.gray(JSON.stringify(extraInfo, null,2)))
+    }
+  })
 }
 
 type NotionClientSingleton = ReturnType<typeof notionClientSingleton>
