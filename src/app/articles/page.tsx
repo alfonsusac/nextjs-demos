f105114703519556994cad01e84246db7b49e7e5
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-20 01:07:09 +0700

    Update page.tsx

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,81 +1,108 @@
 import Link from 'next/link'
 import { cn } from "@/components/typography"
 import { NotionIcon } from "@/components/notion/rsc/images"
-import { TransformedPageData, getArticleList } from "@/components/notion/data/articles"
+import { TransformedNotionPageData, getArticleList } from "@/components/notion/data/articles"
 import supabase from '@/lib/supabase'
 import { delay } from '@/lib/cache'
+import { ErrorBoundary, ErrorBoundaryProps, FallbackProps } from 'react-error-boundary'
+import { Suspense } from 'react'
+import { JSONStringify } from '@/components/tool'
+import { ArticleList_Error } from './client'
+import { memoize } from 'nextjs-better-unstable-cache'
+import { convertObjectArraytoMap } from '@/lib/utils'
 
 export const revalidate = 600 // 10 minutes
 
-export default async function Articles() {
-
-
-
-  const res = await supabase.from("Article").select("*")
-
-  let articles = res.data?.map(a => a.data as TransformedPageData)
-  let prodArticles = articles
-  
-  if (process.env.NODE_ENV === 'development') {
-    console.log("Hello this is node_env dev")
-    await delay(10000)
-    articles = await getArticleList()
+const getArticlesFromSupabase = memoize(
+  async () => (await supabase.from("Article").select("*")).data ?? [],
+  {
+    duration: 3600,
+    revalidateTags: ['articles']
   }
+)
 
-  articles?.sort( (a, b) => parseInt(a.created_time) - parseInt(b.created_time) )
+export default async function ArticlesPage() {
+  console.log("Article List Rendered")
+
+  const res = await fetch("https://random-data-api.com/api/v2/beers", {
+    cache: 'force-cache',
+    next: {
+      tags: ['articles']
+    }
+  })
+  const data = (await res.json()).id as number
+
   return (
-    <div className={ cn(
-      "mx-auto mt-24",
-      "prose-h1:text-5xl",
-      "prose-hr:my-8",
-      "max-w-article"
-    ) }>
-      <h1 className="mb-3">
-        Articles
-      </h1>
-      <p>
-        Articles that I wrote related to Next.js
-      </p>
-      <ul className="p-0 pt-12">
-        {
-          articles?.map(r => (
-            <li key={ r.id } className="list-none m-0 transition-all group -my-2 sm:-mx-8">
+    <div className="mx-auto mt-24   max-w-article   prose-hr:my-8   prose-h1:text-5xl  flex flex-col gap-12">
 
-              <Link
-                href={ `/articles/${r.slug}` }
-                className={ cn(
-                  "block p-4 m-0 rounded-md hover:bg-slate-900/50 no-underline cursor-pointer",
-                  "leading-tight",
-                  "transition-all"
-                ) }
-              >
+      <header>
+        <h1>Articles</h1>
+        <p>Articles that I wrote related to Next.js</p>
+      </header>
 
-                <div className="flex gap-3">
+      <div>
+        {Math.random().toPrecision(3)}
+      </div>
 
-                  <div className="w-5 h-5 text-lg">
-                    <NotionIcon icon={ r.icon } />
-                  </div>
+      <div>
+        { data }
+      </div>
 
-                  <div>
-                    <div className="font-semibold mt-1 group-hover:text-white transition-all">
-                      { r.flattenedTitle }
-                    </div>
-                    <div className="text-xs text-slate-500">
-                      { new Date(r.created_time).toDateString() }
-                      <span className="pl-4">
-                        { (res?.data?.find(a => r.id === a.id)?.views ?? "n/a" )+ " views" }
-                      </span>
-                    </div>
-                  </div>
-
-                </div>
-              </Link>
-
-            </li>
-          ))
-        }
-      </ul>
+      <ArticleList_Error>
+        <Suspense fallback={ <div className="text-sm text-slate-500"> Loading Articles... </div> }>
+          <ArticleList />
+        </Suspense>
+      </ArticleList_Error>
 
     </div>
+  )
+}
+
+async function ArticleList() {
+  // await get article list
+  const list = await memoize(getArticleList, { revalidateTags: ['articles'] })()
+  return (
+    <ul>
+      { list.map(article => <ListItem key={ article.id } article={ article } />) }
+    </ul>
+  )
+}
+
+async function ListItem({ article }: {
+  article: TransformedNotionPageData,
+}) {
+  const metadata = await getArticlesFromSupabase()
+  const Title = "div"
+  const Metadata = "div"
+  return (
+    <li key={ article.id } className="list-none m-0 transition-all group -my-2 sm:-mx-8">
+      <Link
+        href={ `/articles/${article.slug}` }
+        className={ cn(
+          "block p-4 m-0 rounded-md hover:bg-slate-900/50 no-underline cursor-pointer",
+          "leading-tight",
+          "transition-all"
+        ) }
+      >
+        <div className="flex gap-3">
+          {/* LEFT */}
+          <div className="w-5 h-5 text-lg">
+            <NotionIcon icon={ article.icon } />
+          </div>
+          {/* RIGHT */}
+          <div>
+            <Title className="font-semibold mt-1 group-hover:text-white transition-all">
+              { article.flattenedTitle }
+            </Title>
+            <Metadata className="text-xs text-slate-500">
+              { new Date(article.created_time).toDateString() }
+              <span className="pl-4">
+                { (metadata.find(a => article.id === a.id)?.views ?? "0") + " views" }
+              </span>
+            </Metadata>
+          </div>
+        </div>
+      </Link>
+    </li>
   )
 }
