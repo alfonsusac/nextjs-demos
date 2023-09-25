2ede1d162f62f71cee9823acabe6b97e461b6680
Alfonsus Ardani <alfonsusac@gmail.com>
2023-09-25 17:54:27 +1000

    Added maxtime procedure

--- src/lib/procedures.ts
+++ src/lib/procedures.ts
@@ -1,4 +1,5 @@
 import type { Callback } from "@/helper/types"
+import { delay } from "./memoize"
 
 export const Procedure = {
   prepareForStatic<R extends any>(cb: Callback<any, R>, fallback: R) {
@@ -11,7 +12,21 @@ export const Procedure = {
     }
   },
 
-  parallel<T extends unknown[]>(...args: Parameters< typeof Promise.all<T>>) {
+  parallel<T extends unknown[]>(...args: Parameters<typeof Promise.all<T>>) {
     return Promise.all<T>(...args)
+  },
+
+  maxTime<R extends any>(prom: Promise<R>, ms: number) {
+    return Promise.race([
+      (
+        async () => ({ res: await prom, timelimit: false as false })
+      )(),
+      (
+        async () => {
+          await delay(ms)
+          return ({ res: null, timelimit: true as true })
+        }
+      )()
+    ])
   }
 }
